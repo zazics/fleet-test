d850d0be45279f0fe3293b3db7eb58a14bcfef48
Christophe <zazics@users.noreply.github.com>
2026-08-27 10:06:00 +0200

    Add files via upload

---
+++ fleet-domain/src/main/java/be/dieteren/fleet/domain/utils/Currency.java
@@ -0,0 +1,8 @@
+package be.dieteren.fleet.domain.utils;
+
+/**
+ * Represents a currency used in monetary transactions.
+ */
+public enum Currency {
+    USD, EUR
+}
---
+++ fleet-domain/src/main/java/be/dieteren/fleet/domain/utils/Fuel.java
@@ -0,0 +1,8 @@
+package be.dieteren.fleet.domain.utils;
+
+public enum Fuel {
+    PETROL,
+    DIESEL,
+    HYBRID,
+    ELECTRIC
+}
